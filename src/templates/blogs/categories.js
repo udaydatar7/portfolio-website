cc976bc3b4b3ca136f33d65a8dd3de452d9bc195
Uday Datar <udaydatar7@gmail.com>
2021-11-23 17:16:04 +0530

    Update categories.js

--- src/templates/blogs/categories.js
+++ src/templates/blogs/categories.js
@@ -20,9 +20,7 @@ const Categories = ({ data }) => {
       <Intro>
         <ContainerLayout>
 
-          <SubTitle>
-            Articles
-          </SubTitle>
+          
           <HeaderIntro>
             <SubText>
               Articles on front-end design engineering, focused on HTML, CSS, SVG, accessiblity, and everything in between, with practical tips from real projects. Included here are links to articles published on magazines.
